daf81892899cc7c313726a8b9bcfa9cf49ab90ca
Shane Daniel <simrpgman@gmail.com>
2013-12-23 22:05:08 -0600

    .gituserconfig initialization in install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,19 @@ cp -vf ~/.dotfiles/.freshrc ~
 # copy bin/symlinks to ~/bin
 cp -R ~/.dotfiles/bin/symlinks/* ~/bin/
 
+if [[ -r "~/.dotfiles/.gituserconfig" ]]; then
+  echo "Git user config already exists."
+else
+  touch ~/.dotfiles/.gituserconfig
+  echo "### user specific settings" >> ~/.dotfiles/.gituserconfig
+  echo "### e.g." >> ~/.dotfiles/.gituserconfig
+  echo "# [user]" >> ~/.dotfiles/.gituserconfig
+  echo "#   name = John Doe" >> ~/.dotfiles/.gituserconfig
+  echo "#   email = jdoe@example.com" >> ~/.dotfiles/.gituserconfig
+
+  echo "Git user config already exists. Please edit and re-run \`fresh\`."
+fi
+
 # install fresh or run for re-fresh
 if [[ -z `which fresh` ]]; then
   bash -c "`curl -sL get.freshshell.com`"
