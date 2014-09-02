1f2b54288f5c51f6e413e23660c5f2a25a413ecd
Shane Daniel <simrpgman@gmail.com>
2014-09-02 18:45:24 -0500

    remove rvm

--- bash/functions.bash
+++ bash/functions.bash
@@ -125,4 +125,4 @@ fi
 [[ -s $HOME/.nvm/nvm.sh ]] && source $HOME/.nvm/nvm.sh
 
 # Load RVM into a shell session *as a function*
-[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+#[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
