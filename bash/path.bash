f7b633e0fb081e58e2ed13267afa240d138036a0
Shane Daniel <simrpgman@gmail.com>
2014-09-02 13:01:26 -0500

    add some java & maven variables to path for AEM

--- bash/path.bash
+++ bash/path.bash
@@ -10,4 +10,11 @@ PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 # Put ~/bin before everything
 PATH="~/bin:$PATH"
 
+# AEM stuffs
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home"
+export MAVEN_HOME="/usr/local/Cellar/maven/3.2.3/libexec"
+export MAVEN_OPTS="-Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true"
+
+PATH="$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH"
+
 export PATH
