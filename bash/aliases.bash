5e496f201c36de05be6da8ba8d792be327ca115a
Shane Daniel <simrpgman@gmail.com>
2016-01-05 10:18:26 -0800

    add a git changed files edit command

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -101,3 +101,6 @@ alias c='pygmentize -O style=monokai -f console256 -g'
 alias stfu="osascript -e 'set volume output muted true'"
 alias pumpitup="osascript -e 'set volume 10'"
 alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN';"
+
+# open all merge conflicts or currently changed files in sublime text
+alias fixchanges="git diff --name-only | uniq | xargs subl"
