a57b2a1207448d2efa1210e3e736e0c2bf23e6b8
Shane Daniel <simrpgman@gmail.com>
2016-10-27 01:06:06 -0700

    remove generated code; generate a new file with fresh instead of symlinking directly

--- emacs/init.el
+++ emacs/init.el
@@ -11,19 +11,3 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (scpaste smex magit paredit ido-ubiquitous idle-highlight-mode find-file-in-project better-default\
-s))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
