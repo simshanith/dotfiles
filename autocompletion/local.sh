bf1ec660c768f41297d196f90854261639d638c4
Shane Daniel <simrpgman@gmail.com>
2016-10-29 13:54:59 -0700

    check for autocompletion directory

--- autocompletion/local.sh
+++ autocompletion/local.sh
@@ -1,3 +1,5 @@
- for f in /usr/local/etc/bash_completion.d/*; do
- 	source $f;
- done
+if [[ -d "/usr/local/etc/bash_completion.d/" ]]; then
+	for f in /usr/local/etc/bash_completion.d/*; do
+		source $f;
+	done
+fi
