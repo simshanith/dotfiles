b8b845f89fdcac421f6513eff338f0d1885c2ceb
Shane Daniel <simrpgman@gmail.com>
2017-11-18 10:52:35 -0800

    Add iterm shell integration

--- bash/profile.start.bash
+++ bash/profile.start.bash
@@ -10,3 +10,8 @@ shopt -s histappend
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
+
+if [[ "$TERM_PROGRAM" == 'iTerm.app' ]]
+then
+	[[ -r  "~/.iterm2_shell_integration.bash" ]] && source ~/.iterm2_shell_integration.bash
+fi
