91af493640c7fb36fb28204d28683869ee8b510e
Shane Daniel <simrpgman@gmail.com>
2017-11-18 19:46:46 -0800

    Move iTerm shell integration after prompt

--- bash/mathias.dotfiles.bash
+++ bash/mathias.dotfiles.bash
@@ -3,7 +3,7 @@
 # * ~/.extra can be used for other settings you don’t want to commit.
 
 # These are symlinked with fresh.
-for file in ~/.{exports,path,aliases,functions,bash_prompt,extra}; do
+for file in ~/.{exports,path,aliases,functions,bash_prompt,extra,iterm2_shell_integration.bash}; do
     [ -r "$file" ] && source "$file"
 done
 unset file
