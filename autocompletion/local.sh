9038a01789b19fd66397af5110cb26a0f1c870c7
Shane Daniel <simrpgman@gmail.com>
2018-02-08 11:38:34 -0800

    Add bash_completion from brew to profile

--- autocompletion/local.sh
+++ autocompletion/local.sh
@@ -1,3 +1,5 @@
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+
 if [[ -d "/usr/local/etc/bash_completion.d/" ]]; then
 	for f in /usr/local/etc/bash_completion.d/*; do
 		source $f;
