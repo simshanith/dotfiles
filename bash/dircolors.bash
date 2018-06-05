5f1af7355b17ac9c37581efc473b1e22bec1351b
Shane Daniel <shane@fond.co>
2018-06-05 09:50:44 -0700

    Prefer command -v over hash

--- bash/dircolors.bash
+++ bash/dircolors.bash
@@ -4,8 +4,8 @@
 # `brew install coreutils`
 
 # https://github.com/seebi/dircolors-solarized
-if hash gdircolors 2>/dev/null; then
+if [[ -x "$(command -v gdircolors)" ]]; then
   eval `gdircolors ~/.dircolors`
-elif hash dircolors 2>/dev/null; then
+elif [[ -x "$(command -v dircolors)" ]]; then
   eval `dircolors ~/.dircolors`
 fi
